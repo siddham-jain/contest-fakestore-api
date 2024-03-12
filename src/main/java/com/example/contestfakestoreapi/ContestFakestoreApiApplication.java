53c0307571090bc66a39d064b7f9a9adbf077611
siddham-jain <siddhamjain224@gmail.com>
2024-03-12 14:54:24 +0530

    Repo initialization

---
+++ src/main/java/com/example/contestfakestoreapi/ContestFakestoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.contestfakestoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestFakestoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestFakestoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/contestfakestoreapi/ContestFakestoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contestfakestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestFakestoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
